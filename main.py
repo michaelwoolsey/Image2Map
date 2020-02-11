42cb5486893c2418938aa05f9e24233525a2d28d
michaelwoolsey <woolsey7@students.cs.ubc.ca>
2020-02-11 14:04:27 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,107 @@
+from PIL import Image, ImageColor, ImagePalette
+from pathlib import Path
+from PIL.ImageQt import rgb
+from map_colours import MAP_COLOURS
+
+INPUT_FOLDER = Path("input/")
+INPUT_NAME = INPUT_FOLDER / "in.png"
+
+IM_SIZE = 128
+
+
+def load_im():
+	if INPUT_NAME.exists():
+		im = Image.open(INPUT_NAME)
+	elif (INPUT_FOLDER / "in.jpg").exists():
+		im = Image.open(INPUT_FOLDER / "in.jpg")
+	else:
+		im = ""
+		print("Hey no png/jpg exists with the name \"in\"!\nDid you remember to put it in the input folder?")
+		exit()
+	temp = Image.new("RGBA", (128, 128), color=(255, 255, 255))
+
+	if im.size[0] > IM_SIZE or im.size[1] > IM_SIZE:
+		if im.size[0] > im.size[1]:
+			im = im.resize((IM_SIZE, int(im.size[1] * IM_SIZE / im.size[0])), Image.BILINEAR)
+		else:
+			im = im.resize((int(im.size[0] * IM_SIZE / im.size[1]), IM_SIZE), Image.BILINEAR)
+	temp.paste(im, mask=im)
+	im = temp
+	im.save("im.png")
+
+
+def distance(colour1, colour2):
+	(r1, g1, b1, a1) = colour1
+	(r2, g2, b2, a2) = colour2
+	return abs((r1 - r2) * (r1 - r2) + (g1 - g2) * (g1 - g2) + (b1 - b2) * (b1 - b2))
+
+
+def get_closest_to_palette(colour):
+	x = 7000000  # random high number
+	col = (0, 0, 0, 0)
+	for clr in MAP_COLOURS:
+		temp = distance(clr.get("rgba"), colour)
+		if temp <= x:
+			x = temp
+			col = clr.get("rgba")
+	return col
+
+
+def convert_im():
+	IM = Path("im.png")
+	if not IM.exists():
+		print("No file was found with the name \"im.png\"!")
+		exit()
+	im = Image.open(IM)
+	px = im.load()
+
+	pal = []
+	for clr in MAP_COLOURS:
+		pal.append(clr.get("rgba")[0])
+	#for clr in MAP_COLOURS:
+		pal.append(clr.get("rgba")[1])
+	#for clr in MAP_COLOURS:
+		pal.append(clr.get("rgba")[2])
+	print(pal)
+	pal = [int(i*220/255) for i in pal]
+	print(pal)
+	# for clr in MAP_COLOURS:
+	# 	pal.append(clr.get("rgba")[3])
+
+	# im3 = im.copy()
+	#
+	# im3 = im3.quantize(colors=50, method=2, kmeans=0)
+	# #im3.putpalette(pal)
+	# im3.show()
+	#
+	# im5 = Image.new("P", (128, 128), 0)
+	# im5.putpalette(pal)
+	# im5.paste(im3)
+	# im5.show()
+
+	# new_image = Image.new('P', im3.size)
+	# new_image.putpalette(pal * 50)
+	# new_image.paste(im3, (0, 0) + im3.size)
+	# new_image.show()
+
+	# 220/255 is the scale factor for a flat image
+	for x in range(im.size[0]):
+		for y in range(im.size[1]):
+			r, g, b, a = get_closest_to_palette(im.getpixel((x, y)))
+			closest_clr = (int(r*220/255), int(g*220/255), int(b*220/255), 255)
+			px[x, y] = closest_clr
+
+	im.show()
+	im.save("im2.png")
+
+
+load_im()
+convert_im()
+# TODO: add to github
+# TODO: add math functions that will calculate how many resources are needed to build it.
+# first, loop through the image, adding one for each pixel seen with a certain id into an array 51 long
+# then just count, then get the cool totls
+# gotta divide by 64, mod 64 to get num of stacks
+# mining/aquiring stats?
+# TODO: add ability to remove certain blocks/colours
+# TODO: GUI ?????????????
---
+++ map_colours.py
@@ -0,0 +1,338 @@
+MAP_COLOURS = [
+	# {
+	# 	"id": 0,
+	# 	"name": "NONE",
+	# 	"rgba": (0, 0, 0, 0),
+	# 	"blocks": ["air", "barrier", "redstone_lamp", "glass", "iron_bars", "torch"]
+	# },
+	{
+		"id": 1,
+		"name": "GRASS",
+		"rgba": (127, 178, 56, 255),
+		"blocks": ["grass", "slime"]
+	},
+	{
+		"id": 2,
+		"name": "SAND",
+		"rgba": (247, 233, 163, 255),
+		"blocks": ["sand"]
+	},
+	{
+		"id": 3,
+		"name": "WOOL",
+		"rgba": (199, 199, 199, 255),
+		"blocks": ["cobweb"]
+	},
+	{
+		"id": 4,
+		"name": "FIRE",
+		"rgba": (255, 0, 0, 255),
+		"blocks": ["tnt", "redstone_block"]
+	},
+	{
+		"id": 5,
+		"name": "ICE",
+		"rgba": (160, 160, 225, 255),
+		"blocks": ["ice", "packed_ice", "blue_ice"]
+	},
+	{
+		"id": 6,
+		"name": "METAL",
+		"rgba": (167, 167, 167, 255),
+		"blocks": ["iron_block", "brewing_stand", "lantern", "grindstone", "heavy_weighted_pressure_plate"]
+	},
+	{
+		"id": 7,
+		"name": "PLANT",
+		"rgba": (0, 124, 0, 255),
+		"blocks": ["leaves", "cactus", "flower"]
+	},
+	{
+		"id": 8,
+		"name": "SNOW",
+		"rgba": (255, 255, 255, 255),
+		"blocks": ["snow", "snow_layer", "white_wool", "white_carpet", "white_concrete", "white_concrete_powder"]
+	},
+	{
+		"id": 9,
+		"name": "CLAY",
+		"rgba": (164, 168, 184, 255),
+		"blocks": ["clay"]
+	},
+	{
+		"id": 10,
+		"name": "DIRT",
+		"rgba": (151, 109, 77, 255),
+		"blocks": ["dirt", "coarse_dirt", "granite", "polished_granite", "jungle_wood"]
+	},
+	{
+		"id": 11,
+		"name": "STONE",
+		"rgba": (112, 112, 112, 255),
+		"blocks": ["stone", "andesite", "cobblestone", "mossy_cobblestone", "smooth_stone"]
+	},
+	{
+		"id": 12,
+		"name": "WATER",
+		"rgba": (64, 64, 255, 255),  # cannot get in survival
+		"blocks": []
+	},
+	{
+		"id": 13,
+		"name": "WOOD",
+		"rgba": (143, 119, 72, 255),
+		"blocks": ["oak_wood"]
+	},
+	{
+		"id": 14,
+		"name": "QUARTZ",
+		"rgba": (255, 252, 245, 255),
+		"blocks": ["diorite", "polished_diorite", "quartz_block"]
+	},
+	{
+		"id": 15,
+		"name": "COLOR_ORANGE",
+		"rgba": (216, 127, 51, 255),
+		"blocks": [
+			"acacia_wood", "red_sand", "red_sandstone", "orange_wool", "orange_carpet", "orange_concrete",
+			"orange_concrete_powder", "terracotta", "pumpkin", "honey_block"]
+	},
+	{
+		"id": 16,
+		"name": "COLOR_MAGENTA",
+		"rgba": (178, 76, 216, 255),
+		"blocks": ["magenta_wool", "magenta_carpet", "magenta_concrete", "magenta_concrete_powder", "purpur_block"]
+	},
+	{
+		"id": 17,
+		"name": "COLOR_LIGHT_BLUE",
+		"rgba": (102, 153, 216, 255),
+		"blocks": ["light_blue_wool", "light_blue_carpet", "light_blue_concrete", "light_blue_concrete_powder"]
+	},
+	{
+		"id": 18,
+		"name": "COLOR_YELLOW",
+		"rgba": (229, 229, 51, 255),
+		"blocks": ["yellow_wool", "yellow_carpet", "yellow_concrete", "yellow_concrete_powder", "sponge", "hay_block"]
+	},
+	{
+		"id": 19,
+		"name": "COLOR_LIGHT_GREEN",
+		"rgba": (127, 204, 25, 255),
+		"blocks": ["lime_wool", "lime_carpet", "lime_concrete", "lime_concrete_powder", "melon"]
+	},
+	{
+		"id": 20,
+		"name": "COLOR_PINK",
+		"rgba": (242, 127, 165, 255),
+		"blocks": ["pink_wool", "pink_carpet", "pink_concrete", "pink_concrete_powder"]
+	},
+	{
+		"id": 21,
+		"name": "COLOR_GRAY",
+		"rgba": (76, 76, 76, 255),
+		"blocks": ["gray_wool", "gray_carpet", "gray_concrete", "gray_concrete_powder"]
+	},
+	{
+		"id": 22,
+		"name": "COLOR_LIGHT_GRAY",
+		"rgba": (153, 153, 153, 255),
+		"blocks": ["light_grey_wool", "light_grey_carpet", "light_grey_concrete", "light_grey_concrete_powder"]
+	},
+	{
+		"id": 23,
+		"name": "COLOR_CYAN",
+		"rgba": (76, 127, 153, 255),
+		"blocks": ["cyan_wool", "cyan_carpet", "cyan_concrete", "cyan_concrete_powder", "prismarine"]
+	},
+	{
+		"id": 24,
+		"name": "COLOR_PURPLE",
+		"rgba": (127, 63, 178, 255),
+		"blocks": ["purple_wool", "purple_carpet", "purple_concrete", "purple_concrete_powder", "mycelium"]
+	},
+	{
+		"id": 25,
+		"name": "COLOR_BLUE",
+		"rgba": (51, 76, 178, 255),
+		"blocks": ["blue_wool", "blue_carpet", "blue_concrete", "blue_concrete_powder"]
+	},
+	{
+		"id": 26,
+		"name": "COLOR_BROWN",
+		"rgba": (102, 76, 51, 255),
+		"blocks": ["brown_wool", "brown_carpet", "brown_concrete", "brown_concrete_powder", "dark_oak_wood"]
+	},
+	{
+		"id": 27,
+		"name": "COLOR_GREEN",
+		"rgba": (102, 127, 51, 255),
+		"blocks": ["green_wool", "green_carpet", "green_concrete", "green_concrete_powder", "dried_kelp_block"]
+	},
+	{
+		"id": 28,
+		"name": "COLOR_RED",
+		"rgba": (153, 51, 51, 255),
+		"blocks": ["red_wool", "red_carpet", "red_concrete", "red_concrete_powder", "nether_wart_block", "red_mushroom_block"]
+	},
+	{
+		"id": 29,
+		"name": "COLOR_BLACK",
+		"rgba": (25, 25, 25, 255),
+		"blocks": ["black_wool", "black_carpet", "black_concrete", "black_concrete_powder", "obsidian", "coal_block"]
+	},
+	{
+		"id": 30,
+		"name": "GOLD",
+		"rgba": (250, 238, 77, 255),
+		"blocks": ["gold_block", "light_weighted_pressure_plate", "bell"]
+	},
+	{
+		"id": 31,
+		"name": "DIAMOND",
+		"rgba": (92, 219, 213, 255),
+		"blocks": ["diamond_block", "dark_prismarine", "prismarine_bricks"]
+	},
+	{
+		"id": 32,
+		"name": "LAPIS",
+		"rgba": (74, 128, 255, 255),
+		"blocks": ["lapis_block"]
+	},
+	{
+		"id": 33,
+		"name": "EMERALD",
+		"rgba": (0, 217, 58, 255),
+		"blocks": ["emerald_block"]
+	},
+	{
+		"id": 34,
+		"name": "PODZOL",
+		"rgba": (129, 86, 49, 255),
+		"blocks": ["podzol", "spruce_wood"]
+	},
+	{
+		"id": 35,
+		"name": "NETHER",
+		"rgba": (112, 2, 0, 255),
+		"blocks": ["netherrack", "nether_brick", "magma_block", "red_nether_brick"]
+	},
+	{
+		"id": 36,
+		"name": "TERRACOTTA_WHITE",
+		"rgba": (209, 177, 161, 255),
+		"blocks": ["white_terracotta"]
+	},
+	{
+		"id": 37,
+		"name": "TERRACOTTA_ORANGE",
+		"rgba": (159, 82, 36, 255),
+		"blocks": ["orange_terracotta"]
+	},
+	{
+		"id": 38,
+		"name": "TERRACOTTA_MAGENTA",
+		"rgba": (149, 87, 108, 255),
+		"blocks": ["magenta_terracotta"]
+	},
+	{
+		"id": 39,
+		"name": "TERRACOTTA_LIGHT_BLUE",
+		"rgba": (112, 108, 138, 255),
+		"blocks": ["light_blue_terracotta"]
+	},
+	{
+		"id": 40,
+		"name": "TERRACOTTA_YELLOW",
+		"rgba": (186, 133, 36, 255),
+		"blocks": ["yellow_terracotta"]
+	},
+	{
+		"id": 41,
+		"name": "TERRACOTTA_LIGHT_GREEN",
+		"rgba": (103, 117, 53, 255),
+		"blocks": ["lime_terracotta"]
+	},
+	{
+		"id": 42,
+		"name": "TERRACOTTA_pink",
+		"rgba": (160, 77, 78, 255),
+		"blocks": ["pink_terracotta"]
+	},
+	{
+		"id": 43,
+		"name": "TERRACOTTA_GRAY",
+		"rgba": (57, 41, 35, 255),
+		"blocks": ["gray_terracotta"]
+	},
+	{
+		"id": 44,
+		"name": "TERRACOTTA_LIGHT_GRAY",
+		"rgba": (135, 107, 98, 255),
+		"blocks": ["light_gray_terracotta"]
+	},
+	{
+		"id": 45,
+		"name": "TERRACOTTA_CYAN",
+		"rgba": (87, 92, 92, 255),
+		"blocks": ["cyan_terracotta"]
+	},
+	{
+		"id": 46,
+		"name": "TERRACOTTA_PURPLE",
+		"rgba": (122, 73, 88, 255),
+		"blocks": ["purple_terracotta"]
+	},
+	{
+		"id": 47,
+		"name": "TERRACOTTA_BLUE",
+		"rgba": (76, 62, 92, 255),
+		"blocks": ["blue_terracotta"]
+	},
+	{
+		"id": 48,
+		"name": "TERRACOTTA_BROWN",
+		"rgba": (76, 50, 35, 255),
+		"blocks": ["brown_terracotta"]
+	},
+	{
+		"id": 49,
+		"name": "TERRACOTTA_GREEN",
+		"rgba": (76, 82, 42, 255),
+		"blocks": ["green_terracotta"]
+	},
+	{
+		"id": 50,
+		"name": "TERRACOTTA_RED",
+		"rgba": (142, 60, 46, 255),
+		"blocks": ["red_terracotta"]
+	},
+	{
+		"id": 51,
+		"name": "TERRACOTTA_BLACK",
+		"rgba": (37, 22, 16, 255),
+		"blocks": ["black_terracotta"]
+	},
+
+	# {
+	# 	"id":,
+	# 	"name": "TERRACOTTA_",
+	# 	"rgba": (, , , 255),
+	# 	"blocks": ["_terracotta"]
+	# },
+
+	# {
+	# 	"id": ,
+	# 	"name": "",
+	# 	"rgba": (, , , 255),
+	# 	"blocks": []
+	# },
+
+
+	# {
+	# 	"id":,
+	# 	"name": "COLOR_",
+	# 	"rgba": (, , , 255),
+	# 	"blocks": ["_wool", "_carpet", "_concrete", "_concrete_powder"]
+	# },
+]
